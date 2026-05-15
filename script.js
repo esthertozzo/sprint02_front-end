c5c24f4a301e856377e545f95e0cc1a98e171cf9
esther <tozzo.est@gmail.com>
2026-05-15 10:54:45 -0300

    comentarios js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// FORMULÁRIO DE PREFERÊNCIAS DO USUÁRIO com sessionStorage, capturar dados do formulario
+
 const formulario = document.getElementById('formFocus');
 
 if(formulario) {
+    //evento de envio no formulário
     formulario.addEventListener('submit', (event) => {
 
+    // evita o recarregamento da página ao enviar o formulário
     event.preventDefault();
 
+    // cptura os valores que o usuário digitou nos campos do formulário
     const rotina =
     document.getElementById('rotina').value;
 
@@ -13,12 +18,13 @@ if(formulario) {
 
     const exclusao =
     document.getElementById('exclusao').value;
-
+    
     const checkboxes =
     document.querySelectorAll(
         'input[type="checkbox"]:checked'
     );
 
+    // exibe um alerta caso o usuário não tenha preenchido a rotina de estudos
     if(rotina.trim() === ""){
 
         alert(
@@ -28,6 +34,7 @@ if(formulario) {
         return;
     }
 
+    // exibe um alerta caso o usuário não tenha preenchido os checkboxes de prioridades
     if(checkboxes.length === 0){
 
         alert(
@@ -45,12 +52,15 @@ if(formulario) {
         return;
     }
 
+    // array para armazenar prioridades selecionadas pelo usuário
     let prioridades = [];
 
+    // percorre os checkboxes selecionados e adiciona seus valores ao array de prioridades
     checkboxes.forEach(function(item){
         prioridades.push(item.value);
     });
 
+    // objeto com dados
     const usuario = {
         rotina,
         materias,
@@ -58,6 +68,7 @@ if(formulario) {
         prioridades
     };
 
+    // salvar os dados do usuário no sessionStorage
     sessionStorage.setItem(
         "focusUser",
         JSON.stringify(usuario)
@@ -72,6 +83,7 @@ const rotinaUsuario = document.getElementById('rotinaUsuario');
 
 if (rotinaUsuario) {
 
+    // recuperando dados do sessionStorage
     const dados = JSON.parse(sessionStorage.getItem("focusUser"));
 
     if(dados){
@@ -85,10 +97,13 @@ if (rotinaUsuario) {
     }
 }
 
+
+// EXIBIÇÃO DOS DADOS DO USUÁRIO NO DASHBOARD, recuperar as informações salvas no sessionStorage e exibir na tela
 const hero = document.querySelector(".hero");
 
 if(hero){
 
+    //array com imagens para slide
     const imagens = [
         "img/slide00.png",
         "img/slide01.png",
@@ -96,6 +111,7 @@ if(hero){
 
     ];
 
+    //controlador das imagens
     let indice = 0;
 
     const botaoProximo =
@@ -104,6 +120,7 @@ if(hero){
     const botaoAnterior =
     document.getElementById("anterior");
 
+    // evento para avançar (botão)
     botaoProximo.addEventListener("click", () => {
         indice++;
 
